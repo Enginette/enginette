3a6a3285efff6373739a6b9ca1ecbbbaa7bb115e
Vrobocio <87225187+DDev247@users.noreply.github.com>
2022-08-12 00:34:30 +0200

    Create Test.jsx

---
+++ src/pages/Test.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const TestDiv = styled.div`
+    background-color: black;
+    display: flex;
+    justify-content: center;
+
+`;
+
+const Test = () => {
+    return (
+        <TestDiv>
+            <a href="enginette-client://filename=E:\projects\enginette-client\bin\Debug\net48\lawnmower.mr">Test</a>
+        </TestDiv>
+    )
+};
+
+export default Test;
